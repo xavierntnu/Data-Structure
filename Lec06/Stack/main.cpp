cee7fd6b85d9dbf60faac05d64f36ac5c7c6576c
Xavier <40475032H@gmail.com>
2018-02-28 15:52:19 +0800

    Create main.cpp

---
+++ Lec06/Stack/main.cpp
@@ -0,0 +1,43 @@
+#include <Stack.h>
+
+#include <iostream>
+
+using namespace std;
+
+char manual[] = "\n1:push\n2:pop\n3:view\nYour choice: ";
+
+int main()
+{
+//    Stack<int> stackObj(100);
+    Stack<int> StackObj(100);
+    int choice;
+    int push_value, pop_value, *top;
+
+    while(true){
+      cout << manual;
+      cin >> choice;
+
+      switch(choice){
+        case 1:
+          cout << "value: ";
+          cin >> push_value;
+          StackObj.Add(push_value);
+          break;
+
+        case 2:
+          top = StackObj.Delete(pop_value);
+          if(top!=0)
+            cout << "value = " << pop_value << endl;
+          break;
+        case 3:
+          cout << "Please write it by yourself...\n";
+          break;
+
+
+        default:
+          cout << "Invalid choice!\n";
+          break;
+      }
+    }
+    return 0;
+}
